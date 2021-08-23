0b7cdcca4da5d7b07d33004862814202461cb8c7
Taek-min <jtm11245@gmail.com>
2021-08-23 14:48:31 +0900

    no message

---
+++ week1/Day1/Bj_1085.java
@@ -0,0 +1,20 @@
+package algorithm;
+
+import java.util.Scanner;
+
+public class Bj_1085 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		int w = sc.nextInt();
+		int h = sc.nextInt();
+		
+		int min_x = Math.min(x, w-x);	
+		int min_y = Math.min(y, h-y);	
+        
+
+		System.out.println(Math.min(min_x, min_y));
+		sc.close();
+	}
+}
