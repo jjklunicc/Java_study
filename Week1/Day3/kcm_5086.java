0ecd078509cb675e44d4958d8aaf73676cb5893e
ckdals6932 <ckdals6932@naver.com>
2021-08-25 22:18:48 +0900

    Update kcm_5086.java

--- Week1/Day3/kcm_5086.java
+++ Week1/Day3/kcm_5086.java
@@ -8,11 +8,10 @@ public class kcm_5086 {
 		// TODO Auto-generated method stub
 		Scanner in = new Scanner(System.in);
 			while(true) {
-			double num1 = in.nextInt();
-			double num2 = in.nextInt();
-			if(num1 == num2) {
+			int num1 = in.nextInt();
+			int num2 = in.nextInt();
+			if(num1 == num2) 
 				break;
-			}
 			else if(num2 % num1 == 0)
 				System.out.println("factor");
 			else if(num1 % num2 == 0)
